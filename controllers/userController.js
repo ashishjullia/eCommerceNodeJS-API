f78996c5b6bbb36d1f34e748c1e3639140be024b
Gurdeep25 <gscs.engg@gmail.com>
2020-03-21 20:53:16 -0400

    Added signup logic in userController alongwith token generation and for the user being created.

---
+++ controllers/userController.js
@@ -0,0 +1,84 @@
+const User = require('../models/UserModel');
+
+const HttpError = require('../models/HttpErrorModel');
+
+const bcrypt = require('bcryptjs');
+
+const { validationResult } = require('express-validator');
+
+const jsonwebtoken = require('jsonwebtoken');
+
+// CREATE a user
+exports.signUp = async (req, res, next) => {
+    // Validating the data before creating a user
+    const errors = validationResult(req);
+    if (!errors.isEmpty) {
+        return next(
+            new HttpError('Invalid inputs, please provide correct data!', 422)
+        );
+    }
+
+    const { id, firstname, lastname, email, password } = req.body;
+
+    // Check whether user is already registered or not
+    let userExists;
+    try {
+        userExists = await User.findOne({ email: email });
+    } catch (err) {
+        const error = new HttpError('Sign up failed!', 500);
+        // important to return from here.
+        return next(error);         
+    }
+
+    if (userExists) {
+        const error = new HttpError('User already registered with this email, try logging in instead!', 422);
+        return next(error);
+    }
+
+    // Crypt the password before saving it
+    let cryptPassword;
+    // 10 = salting rounds
+    // The has will now return a "promise"
+    try {
+        cryptPassword = await bcrypt.hash(password, 10);
+    } catch (err) {
+        const error = new HttpError('Could not create user, at this time, try again later!', 500);
+        return next(error);
+    } 
+
+    // creating a new user from the data being sent via a post request
+    const newUser = new User({
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        password: cryptPassword,
+    });
+    
+    // save the data to the User model/collection
+    try {
+        const savedUser =  await newUser.save();
+        res.status(201).json(savedUser);
+    } catch (err) {
+        res.json({
+            message: err
+        });
+    }
+
+    // Generate a token
+    let token;
+    try {
+        token = jsonwebtoken.sign(
+            {userId: newUser.id}, 
+            'secret', 
+            {expiresIn: '1h'});
+    } catch (err) {
+        const error = new HttpError('Could not create user, at this time, try again later!', 500);
+        return next(error);
+    } 
+
+    res.status(201).json({
+        userId: newUser.id,
+        email: newUser.email,
+        token: token
+    });
+};
