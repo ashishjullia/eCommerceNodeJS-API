8e6a07eb2939a36f444534821921a1943945a727
Ashish Jullia <ashishjullia19@gmail.com>
2020-03-25 01:00:46 -0400

    Updated the productRoute.

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,14 +3,24 @@ const router = express.Router();
 
 const products_controller = require('../controllers/productController');
 
+const { check } = require('express-validator');
+
 router.get('/', products_controller.getAllProducts);
 
-router.post('/', products_controller.postSingleProduct);
+router.post('/', 
+    [
+        check('title').not().isEmpty(),
+        check('description').not().isEmpty(),
+        check('image').not().isEmpty(),
+        check('pricing').not().isEmpty(),
+        check('shipping').not().isEmpty(),
+        check('quantity').not().isEmpty()
+    ], products_controller.addProduct);
 
-router.get('/:productId', products_controller.getSpecificProduct);
+router.get('/:productId', products_controller.getSpecificProductFromCart);
 
 router.delete('/:productId', products_controller.removeSpecificProduct);
 
-router.patch('/:productId', products_controller.updateSpecificProduct);
+router.patch('/:productId', products_controller.updateSpecificProductInCart);
 
 module.exports = router;
