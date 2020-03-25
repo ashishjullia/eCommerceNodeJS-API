33e2d192060da91746ca7dcf547ba0f30f91031a
Ashish Jullia <ashishjullia19@gmail.com>
2020-03-25 01:42:26 -0400

    Added a basic addressRoute to the application.

---
+++ routes/addressRoute.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const address_controllers = require('../controllers/addressController');
+
+const { check } = require('express-validator');
+
+// GET all addresses
+router.get('/', address_controllers.getAllAddresses);
+
+// ADD an address
+router.post('/',
+    [
+        check('userId').not().isEmpty(),
+        check('street').not().isEmpty(),
+        check('city').not().isEmpty(),
+        check('province').not().isEmpty(),
+        check('postalCode').not().isEmpty(),
+        check('country').not().isEmpty()
+    ],
+    address_controllers.addProductToCart);
+
+// Export
+module.exports = router;
