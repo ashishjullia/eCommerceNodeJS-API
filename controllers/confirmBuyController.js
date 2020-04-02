d634b79810dff6045cefd819b77b454bf11beeb5
Ashish Jullia <ashishjullia19@gmail.com>
2020-04-01 23:22:14 -0400

    Shifted the confirm buy logic inside a function.

--- controllers/confirmBuyController.js
+++ controllers/confirmBuyController.js
@@ -12,47 +12,57 @@ const User = require('../models/UserModel');
 
 exports.confirmBuy = async (req, res, next) => {
 
-    const { userId, token, email } =  req.session;
-    console.log(userId);
-    console.log(token);
-    console.log(email);
+    const { email, token, checkOutItems, address } =  req.session;
 
     try {
         if (token) {
-            const cartObject = await Cart.findOne({ userId: userId });
-            // { cartQuantity, productId }
+            confirmBuyProduct(email, checkOutItems, address, req, res, next);
+        }
+        else {
+            confirmBuyProduct(email, checkOutItems, address, req, res, next);
+        }
+    } catch (err) {
+        res.json({ return: false,
+        message: err.message });
+    }
+};
+
+
+async function confirmBuyProduct(email, checkOutItems, address, req, res, next) { 
+    // const cartObject = await Cart.findOne({ userId: userId });
+            // // { cartQuantity, productId }
             const productObject = await Product.findOne({ _id: cartObject.productId });
-            // { quantity, pricing }
+            // // { quantity, pricing }
 
-            console.log(productObject.quantity);
-            console.log(cartObject.cartQuantity);
-            console.log(productObject.pricing);
-            console.log(cartObject.productId);
+            // console.log(productObject.quantity);
+            // console.log(cartObject.cartQuantity);
+            // console.log(productObject.pricing);
+            // console.log(cartObject.productId);
 
-            if (productObject.quantity > cartObject.cartQuantity) {
+            if (productObject.quantity > checkOutItems.cartQuantity && checkOutItems.cartQuantity > 0) {
                 await Product.updateOne(
-                    { _id: cartObject.productId },
+                    { _id: checkOutItems.productId },
                     { $set: {
-                        quantity: productObject.quantity - cartObject.cartQuantity
+                        quantity: productObject.quantity - checkOutItems.cartQuantity
                     }
                 });
 
                 // Delete from the cart
                 await Cart.deleteOne({ userId: userId });
 
-                const addressObject = await Address.findOne({ userId: userId });
+                // const addressObject = await Address.findOne({ userId: userId });
 
                 // Add to the purchase history
                 const updatePurchaseHistory = new Purchase({
                     username: email,
-                    productId: cartObject.productId,
-                    quantity: cartObject.cartQuantity,
+                    productId: checkOutItems.productId,
+                    quantity: checkOutItems.cartQuantity,
                     price: productObject.pricing,
-                    street: addressObject.street,
-                    city: addressObject.city,
-                    province: addressObject.province,
-                    postalCode: addressObject.postalCode,
-                    country: addressObject.country
+                    street: address.street,
+                    city: address.city,
+                    province: address.province,
+                    postalCode: address.postalCode,
+                    country: address.country
                 });
 
                 const purchased = await updatePurchaseHistory.save();
@@ -69,16 +79,4 @@ exports.confirmBuy = async (req, res, next) => {
                 });
                 next();
             }
-        }
-        else {
-            res.json({
-                return: false,
-                message: "No user is logged in."
-            });
-            next();
-        }
-    } catch (err) {
-        res.json({ return: false,
-        message: err.message });
-    }
-};
+}
